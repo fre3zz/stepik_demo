23ca0ec91b371cbe88277f01eca8b4ae39964597
Nikolay <k0jl9ih@yandex.ru>
2021-07-02 20:46:07 +0300

    mini project

--- conf/urls.py
+++ conf/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from week1 import views
 
 urlpatterns = [
+    path('', views.main, name="main"),
+    path('about/', views.about, name="about"),
+    path('student/<int:student_id>', views.student),
     path('admin/', admin.site.urls),
 ]
--- week1/views.py
+++ week1/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
+
+week = 4
+course = "Django"
+group = "2706"
+students = {
+    1: "ААА",
+    2: "БББ",
+    3: "ВВВ",
+    4: "ГГГ"
+}
+
+def main(request: HttpRequest):
+    return render(request, "week1/index.html", context={
+        "week": week,
+        "course": course,
+        "group": group,
+    })
+
+
+def about(request: HttpRequest):
+    return render(request, "week1/about.html")
+
+def student(request: HttpRequest, student_id):
+    try:
+        student = students[student_id]
+    except KeyError:
+        raise Http404
+
+    return render(request, "week1/student.html", context={
+    'student': student
+    }
+    )
